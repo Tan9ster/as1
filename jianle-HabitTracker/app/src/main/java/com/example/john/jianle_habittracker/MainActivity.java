76ef9c95f8d5ed69a9a00950516bb0d24a311b1e
jianle <jianle@ualberta.ca>
2016-09-24 15:24:35 -0600

    xml interface stuff

---
+++ jianle-HabitTracker/app/src/main/java/com/example/john/jianle_habittracker/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.john.jianle_habittracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+/*on start()
+    do stuff
+    loadfile() this is for starting a file that will contain the history of a habit
+    button add() do stuff
+
+ */
